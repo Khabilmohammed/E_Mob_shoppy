d0e4d6ff9f93e37db9e092c3bff58faddc750e2d
Mohammed khabil <khabilmohammed3@gmail.com>
2023-09-23 18:26:52 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿
+using E_mob_shoppy.Data;
+using E_mob_shoppy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace E_mob_shoppy.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCtegory = _db.Categories.ToList();
+            return View(objCtegory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order and Name should be different");
+                ModelState.AddModelError("DisplayOrder", "Display Order and Name should be different");
+            }
+            if (ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The data is Created";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryfromdb = _db.Categories.FirstOrDefault(u=>u.Category_Id==id);
+            if (categoryfromdb == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order and Name should be different");
+                ModelState.AddModelError("DisplayOrder", "Display Order and Name should be different");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The data is upadated";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryfromdb = _db.Categories.FirstOrDefault(u => u.Category_Id == id);
+            if (categoryfromdb == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeletePost(int? id) { 
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c=>c.Category_Id==id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "The data is Removed";
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
---
+++ Migrations/20230922063803_AddcategoryTableToDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_mob_shoppy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddcategoryTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Category_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Category_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
