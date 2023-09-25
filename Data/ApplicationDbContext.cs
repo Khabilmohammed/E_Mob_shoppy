7a2ce2198c087bafe21872182e2710da465fbf76
Mohammed khabil <khabilmohammed3@gmail.com>
2023-09-25 11:58:28 +0530

    before_n_tire

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using E_mob_shoppy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_mob_shoppy.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category { Category_Id = 1, Name = "Arun", Description = "HHhihkj", DisplayOrder = 1 },
+                new Category { Category_Id = 2, Name = "Arun", Description = "HHhihkj", DisplayOrder = 1 },
+                new Category { Category_Id = 3, Name = "Arun", Description = "HHhihkj", DisplayOrder = 1 }
+                );
+        }
+    }
+}
