7d1973b7d9387f432b7d3eb40bcca5ee50c627dd
Mohammed khabil <khabilmohammed3@gmail.com>
2023-10-08 17:29:33 +0530

    roles_assigning_completed

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -23,6 +23,17 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
             return View(productList);
         }
 
+
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.ProductId==id,includeProperties: "Category");
+            return View(product);
+        }
+
+
+
+
         public IActionResult Privacy()
         {
             return View();
--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using E_mob_shoppy.DataAccess.Data;
 using E_mob_shoppy.DataAccess.Repository;
 using E_mob_shoppy.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using E_mob_shoppy.Utility;
 
 namespace E_mob_shoppy
 {
@@ -17,8 +21,10 @@ namespace E_mob_shoppy
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+            builder.Services.AddRazorPages();
             builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
-
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -33,9 +39,9 @@ namespace E_mob_shoppy
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
