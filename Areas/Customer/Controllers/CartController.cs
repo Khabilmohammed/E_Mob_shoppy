647ddd8548b961d125e40c7253b4ac17d5a99b4b
Mohammed khabil <khabilmohammed3@gmail.com>
2023-10-31 15:25:47 +0530

    Coupon_completed

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -40,26 +40,26 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
                 cart.Price = GetPriceBasedOnQuatity(cart);
                 shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.count);
             }
- 
             return View(shoppingCartVM);
         }
+
+
 
         public ActionResult Summary()
         {
 
-
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            shoppingCartVM = new()
-            {
-                ShoppingCartList = _unitOfWork.  ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
-                OrderHeader = new()
 
-            };
+			shoppingCartVM = new()
+			{
+				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+				OrderHeader = new()
+			};
 
-            shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
-
+            shoppingCartVM.OrderHeader.ApplicationUserId=userId;
+			shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
             shoppingCartVM.OrderHeader.Name=shoppingCartVM.OrderHeader.ApplicationUser.Name;
             shoppingCartVM.OrderHeader.PhoneNumber = shoppingCartVM.OrderHeader.PhoneNumber;
             shoppingCartVM.OrderHeader.streetAddress = shoppingCartVM.OrderHeader.streetAddress;
@@ -67,20 +67,188 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
             shoppingCartVM.OrderHeader.postalCode = shoppingCartVM.OrderHeader.postalCode;
             shoppingCartVM.OrderHeader.state = shoppingCartVM.OrderHeader.state;
             
-
-
-            foreach (var cart in shoppingCartVM.ShoppingCartList)
-            {
-                cart.Price = GetPriceBasedOnQuatity(cart);
-                shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.count);
-            }
+            
+           
+          
+                foreach (var cart in shoppingCartVM.ShoppingCartList)
+                {
+                    cart.Price = GetPriceBasedOnQuatity(cart);
+                    shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.count);
+                }
+               
 
             return View(shoppingCartVM);
         }
 
 
+		/*[HttpPost]
+        public IActionResult ApplyCoupon(string couponCode)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+                OrderHeader = new()
+            };
 
-        [HttpPost]
+            shoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+            shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+
+            shoppingCartVM.OrderHeader.Name = shoppingCartVM.OrderHeader.ApplicationUser.Name;
+            shoppingCartVM.OrderHeader.PhoneNumber = "phone Number";
+            shoppingCartVM.OrderHeader.streetAddress = "Address";
+            shoppingCartVM.OrderHeader.City = "city";
+            shoppingCartVM.OrderHeader.postalCode = "Postal code";
+            shoppingCartVM.OrderHeader.state = "state";
+
+            var validCoupon = _unitOfWork.Coupon.Get(c => c.Code == couponCode);
+            if (validCoupon != null)
+            {
+                shoppingCartVM.OrderHeader.AppliedCouponCode = validCoupon.Code;
+                shoppingCartVM.OrderHeader.DiscountAmount = validCoupon.DiscountAmount;
+
+               
+                shoppingCartVM.OrderHeader.OrderTotal = CalculateOrderTotalWithCoupon(shoppingCartVM);
+                _unitOfWork.OrderHeader.Upadte(shoppingCartVM.OrderHeader);
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Summary));
+            }
+            else
+            {
+                // Handle invalid coupon code
+                TempData["CouponError"] = "Invalid coupon code.";
+                return RedirectToAction(nameof(Index));
+            }
+
+        }*/
+
+
+
+		/*  private double CalculateOrderTotalWithCoupon(ShoppingCartVM cartVM)
+		  {
+
+			  // Calculate the order total with the coupon discount
+			  foreach (var cart in shoppingCartVM.ShoppingCartList)
+			  {
+				  cart.Price = GetPriceBasedOnQuatity(cart);
+				  shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.count);
+			  }
+			  double orderTotal = shoppingCartVM.OrderHeader.OrderTotal;
+
+			  if (!string.IsNullOrEmpty(cartVM.OrderHeader?.AppliedCouponCode) && cartVM.OrderHeader.DiscountAmount != null)
+			  {
+				  // Apply the coupon discount if a valid coupon is applied
+				  orderTotal -= cartVM.OrderHeader.DiscountAmount.Value;
+			  }
+
+			  return orderTotal;
+		  }*/
+		public  IActionResult Coupon(string coupon, int? OrderTotal)
+		{
+			if (string.IsNullOrEmpty(coupon) || OrderTotal == null)
+			{
+				return BadRequest(); // Return an appropriate error response
+			}
+
+			var validCoupon =  _unitOfWork.Coupon.Get(u => u.Code == coupon);
+
+			if (validCoupon != null)
+			{
+				if (OrderTotal >= validCoupon.DiscountAmount)
+				{
+					double discountPrice;
+					double cartTotal = Convert.ToDouble(OrderTotal);
+					if (validCoupon.DiscountAmount > 0)
+					{
+						discountPrice = (double)(cartTotal - validCoupon.DiscountAmount);
+					}
+					else
+					{
+						discountPrice = (double)(cartTotal - (cartTotal) * (validCoupon.DiscountAmount / 100));
+					}
+
+				     double newTotal = (double)(OrderTotal - discountPrice);
+
+					var response = new
+					{
+						success = true,
+						discountPrice,
+						newTotal
+					};
+
+					return Json(response); // Return the discount price
+				}
+				else
+				{
+					TempData["error"] = "Order total is below the minimum purchase amount.";
+
+					var responses = new
+					{
+						success = false,
+						errorMessage = "Order total is below the minimum purchase amount."
+					};
+					return Json(responses);
+					// Return an appropriate error response
+				}
+			}
+			TempData["error"] = "Coupon not found.";
+			var responsed = new
+			{
+				success = false,
+				errorMessage = "Coupon not found"
+			};
+			return Json(responsed);
+		}
+
+
+
+		public  double CouponCheckout(string coupon, int? OrderTotal)
+		{
+			if (string.IsNullOrEmpty(coupon) || OrderTotal == null)
+			{
+				return 0; // Return an appropriate error response
+			}
+
+			var couponObj =  _unitOfWork.Coupon.Get(u => u.Code == coupon);
+
+			if (couponObj != null)
+			{
+				if (OrderTotal >= couponObj.DiscountAmount)
+				{
+					double newTotal;
+					double cartTotal = Convert.ToDouble(OrderTotal);
+					if (couponObj.DiscountAmount > 0)
+					{
+						newTotal = (double)(cartTotal - couponObj.DiscountAmount);
+					}
+					else
+					{
+						newTotal = (double)(cartTotal - (cartTotal) * (couponObj.DiscountAmount / 100));
+					}
+
+					double discountPrice = (double)(OrderTotal - newTotal);
+
+					return discountPrice; // Return the discount price
+				}
+				else
+				{
+					TempData["error"] = "Order total is below the minimum purchase amount.";
+					return 0;
+					// Return an appropriate error response
+				}
+			}
+			TempData["error"] = "Coupon not found.";
+			return 0;
+		}
+
+
+
+
+
+		[HttpPost]
         [ActionName("Summary")]
 		public ActionResult SummaryPost()
 		{
@@ -102,10 +270,10 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
 				cart.Price = GetPriceBasedOnQuatity(cart);
 				shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.count);
 			}
+			
 
-           
 
-            shoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+			shoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
             shoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
             _unitOfWork.OrderHeader.Add(shoppingCartVM.OrderHeader);
             _unitOfWork.Save();
@@ -125,24 +293,59 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
 				_unitOfWork.OrderDetail.Add(orderDetail);
                 _unitOfWork.Save();
 			}
+			if (!string.IsNullOrEmpty(shoppingCartVM.OrderHeader.AppliedCouponCode))
+			{
+				var couponResponse = CouponCheckout(shoppingCartVM.OrderHeader.AppliedCouponCode, (int)shoppingCartVM.OrderHeader.OrderTotal);
 
+				if (couponResponse != null)
+				{
+					// Update the order total with the new total
+					shoppingCartVM.OrderHeader.DiscountAmount = couponResponse;
+					shoppingCartVM.OrderHeader.OrderTotal -= (double)couponResponse;
 
+				}
+				else
+				{
+					// Handle the error case when the coupon is not valid or the order total is below the minimum purchase amount
+
+					return RedirectToAction(nameof(Index));
+				}
+			}
+			double totalAmountStripe = shoppingCartVM.OrderHeader.OrderTotal;
 			var domain = "https://localhost:44346/";
 			var options = new SessionCreateOptions
 			{
-				SuccessUrl = domain + $"customer/cart/OrderConformation?id={shoppingCartVM.OrderHeader.OrderHeaderId}",
-				CancelUrl = domain + $"customer/cart/Index",
-				LineItems = new List<SessionLineItemOptions>(),
+				SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={shoppingCartVM.OrderHeader.OrderHeaderId}",
+				CancelUrl = domain + "customer/cart/Index",
+
+				LineItems = new List<SessionLineItemOptions>
+				{
+					new SessionLineItemOptions
+					{
+						PriceData = new SessionLineItemPriceDataOptions
+						{
+							Currency = "usd",
+							UnitAmount = (long)(totalAmountStripe * 100),
+							ProductData = new SessionLineItemPriceDataProductDataOptions
+							{
+								Name = "E_MOb_Shoppy",
+								Description = "Paying through Stripe"
+							}
+						},
+						Quantity = 1
+					}
+				},
 				Mode = "payment",
 			};
 
-			foreach (var item in shoppingCartVM.ShoppingCartList)
+			/*foreach (var item in shoppingCartVM.ShoppingCartList)
 			{
 				var sessionLineItem = new SessionLineItemOptions
 				{
 					PriceData = new SessionLineItemPriceDataOptions
 					{
-						UnitAmount = (long)(item.Price * 100),
+						UnitAmount = (long)(totalAmountStripe * 100),
+
 						Currency = "usd",
 						ProductData = new SessionLineItemPriceDataProductDataOptions
 						{
@@ -152,15 +355,14 @@ namespace E_mob_shoppy.Areas.Customer.Controllers
 					Quantity = item.count
 				};
 				options.LineItems.Add(sessionLineItem);
-			}
+			}*/
 			var service = new SessionService();
-			Session session = service.Create(options);
-			_unitOfWork.OrderHeader.UpdateStripePaymentId(shoppingCartVM.OrderHeader.OrderHeaderId, session.Id, session.PaymentIntentId);
-			_unitOfWork.Save();
-			Response.Headers.Add("Location", session.Url);
-			return new StatusCodeResult(303);
-
-			return RedirectToAction(nameof(OrderConformation),new {id=shoppingCartVM.OrderHeader.OrderHeaderId}); 
+				Session session = service.Create(options);
+				_unitOfWork.OrderHeader.UpdateStripePaymentId(shoppingCartVM.OrderHeader.OrderHeaderId, session.Id, session.PaymentIntentId);
+				_unitOfWork.Save();
+				Response.Headers.Add("Location", session.Url);
+				return new StatusCodeResult(303);
+				return RedirectToAction(nameof(OrderConformation), new { id = shoppingCartVM.OrderHeader.OrderHeaderId });
 		}
 
 
